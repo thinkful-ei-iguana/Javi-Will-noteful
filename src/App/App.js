6e7c6a07e64b22fc68a178a9aa54087b3b7c6642
stephenschab <sschab689@gmail.com>
2019-11-20 10:40:44 -0700

    added handleDeleteNote function to App.js

--- src/App/App.js
+++ src/App/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     handleDeleteNote = (noteId) => {
       this.setState({
         notes: this.state.notes.filter(note => note.id !== noteId)
-      })
+      });
     }
 
     renderNavRoutes() {
